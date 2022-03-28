b5faf6ae3a94c0f4a78e37381a4b18e3629326a1
shotanue <shotanue@gmail.com>
2022-03-29 02:02:12 +0900

    chore: add plugin:import/recommended

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -5,7 +5,8 @@ module.exports = {
         node: true
     },
     extends:[
-        'standard'
+        'standard',
+        'plugin:import/recommended',
     ],
     ignorePatterns: [
         '*.min.*',
