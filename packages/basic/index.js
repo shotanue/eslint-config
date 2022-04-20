a771b596e73217acb8f9dcb86e2546d47d4beb95
shotanue <shotanue@gmail.com>
2022-04-21 07:59:35 +0900

    feat: update basic rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,20 +1,24 @@
 module.exports = {
-    env: {
-        es6: true,
-        browser: true,
-        node: true
-    },
-    extends:[
-        'standard',
-        'plugin:import/recommended',
-    ],
-    ignorePatterns: [
-        '*.min.*',
-        'dist',
-        'public',
-        'coverage',
-        'packages-lock.json',
-        'pnpm-lock.yaml',
-        'yarn.lock',
-    ]
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:import/recommended'
+  ],
+  ignorePatterns: [
+    '*.min.*',
+    'dist',
+    'public',
+    'coverage',
+    'packages-lock.json',
+    'pnpm-lock.yaml',
+    'yarn.lock'
+  ],
+  rules: {
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
+    indent: ['error', 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }]
+  }
 }
