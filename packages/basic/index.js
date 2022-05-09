a9c55a91315df46deabd9c1826b0b6994a6ffa5f
shotanue <shotanue@gmail.com>
2022-05-10 07:26:02 +0900

    chore: add import rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -23,6 +23,13 @@ module.exports = {
     }
   },
   rules: {
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+        
     'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
     indent: ['error', 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }]
   }
